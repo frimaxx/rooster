b6282357d9d2e33e07bda48736e6f95e472a22bf
Frits <frits.vanturnhout@gmail.com>
2020-08-11 11:52:00 +0200

    user list overflow and user deletion bug

--- app/Models/Branch.php
+++ app/Models/Branch.php
@@ -28,6 +28,7 @@ class Branch extends Model
     public function getEmployees($branch_id) {
         return $this->select('branches.name as branch_name', 'users.name', 'users.username', 'users.id as user_id', 'users.uren_min', 'users.uren_max', 'users.avatar')
             ->where('branches.id', $branch_id)
+            ->whereNull('users.deleted_at')
             ->join('employees', 'employees.branch_id', '=', 'branches.id')
             ->join('users', 'employees.user_id', '=', 'users.id');
     }
